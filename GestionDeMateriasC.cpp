b493e51f3c41f78af4161347d58e8e48ef780562
Jose Jurado Cortés <jjuracor@myuax.com>
2023-12-14 12:14:34 +0100

    commit 3

--- GestionDeMateriasC.cpp
+++ GestionDeMateriasC.cpp
@@ -35,3 +35,20 @@ void eliminarMateria(struct Estudiante* estudiante, const char* nombreMateria) {
         }
     }
 }
+// Función para mostrar las materias del estudiante
+void mostrarMaterias(const struct Estudiante* estudiante) {
+    printf("Materias de %s:\n", estudiante->nombre);
+    for (int i = 0; i < estudiante->numMaterias; ++i) {
+        printf("Nombre: %s, Calificación: %.2f\n", estudiante->materias[i].nombre, estudiante->materias[i].calificacion);
+    }
+}
+
+int main() {
+    // Crear un objeto de la estructura Estudiante
+    struct Estudiante estudiante1;
+
+    // Asignar valores a los campos del estudiante
+    snprintf(estudiante1.nombre, sizeof(estudiante1.nombre), "Juan Perez");
+    estudiante1.edad = 20;
+    estudiante1.promedio = 8.5;
+    estudiante1.numMaterias = 0;
