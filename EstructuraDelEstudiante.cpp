10ce92b7ba41d04957a245c1b7892c0ca28762de
Jose Jurado Cortés <jjuracor@myuax.com>
2023-12-14 11:55:19 +0100

    commit 2

--- EstructuraDelEstudiante.cpp
+++ EstructuraDelEstudiante.cpp
@@ -9,3 +9,9 @@ struct Estudiante {
     int edad;
     float promedio;
 };
+// Función para mostrar los campos de la estructura Estudiante
+void mostrarEstudiante(const Estudiante& estudiante) {
+    cout << "Nombre: " << estudiante.nombre << endl;
+    cout << "Edad: " << estudiante.edad << " años" << endl;
+    cout << "Promedio: " << estudiante.promedio << endl;
+}
