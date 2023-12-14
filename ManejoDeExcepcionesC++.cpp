e921b64df6dd5dd2d738cc81dbc0adc03f2c285c
Jose Jurado Cortés <jjuracor@myuax.com>
2023-12-14 12:32:02 +0100

    commit 2

--- ManejoDeExcepcionesC++.cpp
+++ ManejoDeExcepcionesC++.cpp
@@ -17,3 +17,22 @@ class FormatoFechaInvalido : public invalid_argument {
 public:
     FormatoFechaInvalido(const string& mensaje) : invalid_argument(mensaje) {}
 };
+// Excepción personalizada para materias no registradas
+class MateriaNoRegistrada : public logic_error {
+public:
+    MateriaNoRegistrada(const string& mensaje) : logic_error(mensaje) {}
+};
+
+// Definición de la estructura Materia
+struct Materia {
+    string nombre;
+    float calificacion;
+};
+
+// Definición de la estructura Estudiante
+struct Estudiante {
+    string nombre;
+    int edad;
+    float promedio;
+    vector<Materia> materias;
+    vector<Asistencia> asistencias;
