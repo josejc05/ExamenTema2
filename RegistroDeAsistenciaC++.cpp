d42b3efe1e83f14e4574eb6cb897b5b9c411ca9a
Jose Jurado Cortés <jjuracor@myuax.com>
2023-12-14 12:23:26 +0100

    commit 1

---
+++ RegistroDeAsistenciaC++.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+// Definición de la estructura Asistencia
+struct Asistencia {
+    string fecha;
+    string materia;
+    string estado;
+};
+
+// Definición de la estructura Materia
+struct Materia {
+    string nombre;
+    float calificacion;
+};
