2bb67e3984bbe4bab75879e8153e2be42fa32781
Jose Jurado Cortés <jjuracor@myuax.com>
2023-12-14 12:16:50 +0100

    commit 6

--- GestionDeMateriasC.cpp
+++ GestionDeMateriasC.cpp
@@ -15,3 +15,23 @@ struct Estudiante {
     struct Materia materias[10]; // Lista de materias
     int numMaterias;
 };
+// Función para agregar una materia al estudiante
+void agregarMateria(struct Estudiante* estudiante, const struct Materia* materia) {
+    if (estudiante->numMaterias < 10) {
+        estudiante->materias[estudiante->numMaterias++] = *materia;
+    }
+}
+
+// Función para eliminar una materia del estudiante
+void eliminarMateria(struct Estudiante* estudiante, const char* nombreMateria) {
+    for (int i = 0; i < estudiante->numMaterias; ++i) {
+        if (strcmp(estudiante->materias[i].nombre, nombreMateria) == 0) {
+            // Eliminar la materia moviendo las restantes
+            for (int j = i; j < estudiante->numMaterias - 1; ++j) {
+                estudiante->materias[j] = estudiante->materias[j + 1];
+            }
+            estudiante->numMaterias--;
+            break;
+        }
+    }
+}
