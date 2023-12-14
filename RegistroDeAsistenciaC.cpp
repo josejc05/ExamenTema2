6f14d2c06f4c13a6c82faad42f9ff3a9cf7940e4
Jose Jurado Cortés <jjuracor@myuax.com>
2023-12-14 12:26:57 +0100

    commit 3

--- RegistroDeAsistenciaC.cpp
+++ RegistroDeAsistenciaC.cpp
@@ -32,3 +32,20 @@ struct Estudiante {
             numAsistencias++;
         }
     }
+    // Función para mostrar la asistencia
+    void mostrarAsistencia() const {
+        printf("Registro de Asistencia para %s:\n", nombre);
+        for (int i = 0; i < numAsistencias; ++i) {
+            printf("Fecha: %s, Materia: %s, Estado: %s\n", asistencias[i].fecha, asistencias[i].materia, asistencias[i].estado);
+        }
+    }
+};
+
+int main() {
+    // Crear un objeto de la estructura Estudiante
+    struct Estudiante estudiante1;
+    snprintf(estudiante1.nombre, sizeof(estudiante1.nombre), "Juan Perez");
+    estudiante1.edad = 20;
+    estudiante1.promedio = 8.5;
+    estudiante1.numMaterias = 0;
+    estudiante1.numAsistencias = 0;
