3c394a3313668914d0f15b1ea89ff1fbb29bfcef
Jose Jurado Cortés <jjuracor@myuax.com>
2023-12-14 12:23:55 +0100

    commit 2

--- RegistroDeAsistenciaC++.cpp
+++ RegistroDeAsistenciaC++.cpp
@@ -16,3 +16,25 @@ struct Materia {
     string nombre;
     float calificacion;
 };
+// Definición de la estructura Estudiante
+struct Estudiante {
+    string nombre;
+    int edad;
+    float promedio;
+    vector<Materia> materias;
+    vector<Asistencia> asistencias;
+
+    // Función para registrar la asistencia
+    void registrarAsistencia(const string& fecha, const string& materia, const string& estado) {
+        Asistencia nuevaAsistencia = {fecha, materia, estado};
+        asistencias.push_back(nuevaAsistencia);
+    }
+
+    // Función para mostrar la asistencia
+    void mostrarAsistencia() const {
+        cout << "Registro de Asistencia para " << nombre << ":" << endl;
+        for (const auto& asistencia : asistencias) {
+            cout << "Fecha: " << asistencia.fecha << ", Materia: " << asistencia.materia << ", Estado: " << asistencia.estado << endl;
+        }
+    }
+};
