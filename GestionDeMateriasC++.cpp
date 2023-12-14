3731581a608ce35b1148d31571b6e03ac2ac0f1e
Jose Jurado Cortés <jjuracor@myuax.com>
2023-12-14 12:11:47 +0100

    commit 4

--- GestionDeMateriasC++.cpp
+++ GestionDeMateriasC++.cpp
@@ -47,3 +47,19 @@ int main() {
     estudiante1.nombre = "Juan Perez";
     estudiante1.edad = 20;
     estudiante1.promedio = 8.5;
+    // Agregar materias al estudiante
+    agregarMateria(estudiante1, {"Matemáticas", 9.0});
+    agregarMateria(estudiante1, {"Historia", 8.0});
+    agregarMateria(estudiante1, {"Programación", 7.5});
+
+    // Mostrar las materias del estudiante
+    mostrarMaterias(estudiante1);
+
+    // Eliminar una materia del estudiante
+    eliminarMateria(estudiante1, "Historia");
+
+    // Mostrar las materias actualizadas del estudiante
+    mostrarMaterias(estudiante1);
+
+    return 0;
+}
