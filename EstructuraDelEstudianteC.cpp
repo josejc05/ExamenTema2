607d0625578b9c911914907678c58005e4171460
Jose Jurado Cortés <jjuracor@myuax.com>
2023-12-14 11:59:05 +0100

    commit 2

--- EstructuraDelEstudianteC.cpp
+++ EstructuraDelEstudianteC.cpp
@@ -13,3 +13,17 @@ void mostrarEstudiante(const struct Estudiante* estudiante) {
     printf("Edad: %d años\n", estudiante->edad);
     printf("Promedio: %.2f\n", estudiante->promedio);
 }
+int main() {
+    // Crear un objeto de la estructura Estudiante
+    struct Estudiante estudiante1;
+
+    // Asignar valores a los campos
+    snprintf(estudiante1.nombre, sizeof(estudiante1.nombre), "Juan Perez");
+    estudiante1.edad = 20;
+    estudiante1.promedio = 8.5;
+
+    // Llamar a la función mostrarEstudiante para mostrar los campos
+    mostrarEstudiante(&estudiante1);
+
+    return 0;
+}
