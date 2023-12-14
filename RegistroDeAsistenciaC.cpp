6f576e16fd04b2447880c4fc703cae0e640b922a
Jose Jurado Cortés <jjuracor@myuax.com>
2023-12-14 12:26:30 +0100

    commit 2

--- RegistroDeAsistenciaC.cpp
+++ RegistroDeAsistenciaC.cpp
@@ -13,3 +13,22 @@ struct Materia {
     char nombre[50];
     float calificacion;
 };
+// Definición de la estructura Estudiante
+struct Estudiante {
+    char nombre[50];
+    int edad;
+    float promedio;
+    struct Materia materias[10];
+    int numMaterias;
+    struct Asistencia asistencias[50];
+    int numAsistencias;
+
+    // Función para registrar la asistencia
+    void registrarAsistencia(const char* fecha, const char* materia, const char* estado) {
+        if (numAsistencias < 50) {
+            snprintf(asistencias[numAsistencias].fecha, sizeof(asistencias[numAsistencias].fecha), "%s", fecha);
+            snprintf(asistencias[numAsistencias].materia, sizeof(asistencias[numAsistencias].materia), "%s", materia);
+            snprintf(asistencias[numAsistencias].estado, sizeof(asistencias[numAsistencias].estado), "%s", estado);
+            numAsistencias++;
+        }
+    }
