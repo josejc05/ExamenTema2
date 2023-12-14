91ba49cbb98dfa66d054d0ec9b0dab4cebbb8cfd
Jose Jurado Cortés <jjuracor@myuax.com>
2023-12-14 11:54:53 +0100

    commit 1

---
+++ EstructuraDelEstudiante.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+// Definición de la estructura Estudiante
+struct Estudiante {
+    string nombre;
+    int edad;
+    float promedio;
+};
