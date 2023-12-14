5e44de455189cdb080929bd9fec581e4ec5304ed
Jose Jurado Cortés <jjuracor@myuax.com>
2023-12-14 12:36:29 +0100

    commit 2

--- ManejoDeExcepcionesC.cpp
+++ ManejoDeExcepcionesC.cpp
@@ -13,3 +13,19 @@ struct Materia {
     char nombre[50];
     float calificacion;
 };
+// Definición de la estructura Estudiante
+struct Estudiante {
+    char nombre[50];
+    int edad;
+    float promedio;
+    struct Materia materias[10];
+    int numMaterias;
+    struct Asistencia asistencias[50];
+    int numAsistencias;
+};
+
+// Función para registrar la asistencia con manejo de errores
+int registrarAsistencia(struct Estudiante* estudiante, const char* fecha, const char* materia, const char* estado) {
+    // Verificar el formato de la fecha (por ejemplo, debería ser "YYYY-MM-DD")
+    if (strlen(fecha) != 10 || fecha[4] != '-' || fecha[7] != '-') {
+        printf("Error: Formato de fecha inválido\n");
